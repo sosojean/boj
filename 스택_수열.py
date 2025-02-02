33e470b1739da6975afa7e7c1d9705e3fe68284c
sosojean <thwlsdl0989@gmail.com>
2025-02-02 15:41:07 +0900

    스택_스택 수열

---
+++ 스택_수열.py
@@ -0,0 +1,46 @@
+n = int(input())
+
+stack = []
+
+for i in range(n):
+    stack.append(int(input()))
+
+
+# print(stack)
+curr = 0
+
+new_stack = []
+ans_list = []
+
+for i in stack :
+
+    while(i > curr):
+        curr +=1
+
+        new_stack.append(curr)
+        # print(new_stack, curr,i)
+
+        ans_list.append('+')
+
+    if i == new_stack[len(new_stack)-1] :
+
+        new_stack.pop()
+        ans_list.append('-')
+
+
+
+if len(new_stack) > 0 :
+    print('No')
+else :
+    for ans in ans_list:
+        print(ans)
+
+# print(len(new_stack))
+
+
+
+
+
+
+
+
